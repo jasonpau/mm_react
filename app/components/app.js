2edac1b53e021c2d93af4df554c0bc4486a7ac23
Jason Pau <jason@jasonpau.com>
2017-06-15 18:21:09 -0700

    First commit! Yay!

---
+++ app/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>This is not a test.</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem et iusto neque nostrum. Adipisci aperiam commodi culpa dicta doloribus, enim error esse eveniet harum, inventore iure labore nemo nihil numquam odio, recusandae rem sint suscipit tempora voluptas! Aliquid, nesciunt voluptatibus.</p>
+        </div>
+    )
+};
+
+export default App;
